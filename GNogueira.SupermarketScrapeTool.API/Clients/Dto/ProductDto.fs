657459a481d91411b92dc45cc7c4bda9ab662859
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2023-09-19 22:22:06 +0100

    refactor: add ProductClient

---
+++ GNogueira.SupermarketScrapeTool.API/Clients/Dto/ProductDto.fs
@@ -0,0 +1,9 @@
+﻿namespace GNogueira.SupermarketScrapeTool.API.Clients
+
+open System
+
+type ProductDto =
+    { Id: Guid
+      Name: string
+      Price: float
+      PriceUnit: string }
---
+++ GNogueira.SupermarketScrapeTool.API/Clients/ProductClient.fs
@@ -0,0 +1,43 @@
+﻿namespace GNogueira.SupermarketScrapeTool.API.Clients
+
+open FsToolkit.ErrorHandling
+open GNogueira.SupermarketScrapeTool.API
+open Microsoft.Azure.Cosmos
+open Microsoft.Azure.Cosmos.Linq
+open System.Linq
+open Microsoft.FSharp.Control
+
+type IProductClient =
+    abstract GetById: string -> Async<Result<ProductDto, exn>>
+    abstract GetPaged: count: int -> page: int -> Async<Result<ProductDto seq, exn>>
+
+type ProductClient(cosmosDbClient: ICosmosDbClient) =
+    let productContainer = cosmosDbClient.DefaultContainer
+
+    interface IProductClient with
+        member this.GetById id =
+            cosmosDbClient.DefaultContainer.ReadItemAsync<ProductDto>(id, PartitionKey id)
+            |> AsyncResult.ofTask
+            |> AsyncResult.map (fun ir -> ir.Resource)
+
+        member this.GetPaged count page =
+            async {
+                let queryable = productContainer.GetItemLinqQueryable<ProductDto>()
+
+                let matches = queryable.Skip(count * page).Take(count)
+
+                use linqFeed = matches.ToFeedIterator()
+
+                let rec readResultsAsync acc =
+                    async {
+                        if linqFeed.HasMoreResults then
+                            let! response = linqFeed.ReadNextAsync() |> Async.AwaitTask
+                            return! acc @ (response.Resource |> Seq.toList) |> readResultsAsync
+                        else
+                            return acc
+                    }
+
+                let! result = readResultsAsync []
+
+                return! result |> List.toSeq |> Ok |> Async.retn
+            }
