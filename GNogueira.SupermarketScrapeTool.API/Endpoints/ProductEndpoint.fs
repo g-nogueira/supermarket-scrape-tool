2b6754a6e5f03bc9a4331a2f00cd9558df596d11
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2023-09-19 22:25:01 +0100

    feat: add endpoints getProductById and getProductsPaged

--- GNogueira.SupermarketScrapeTool.API/Endpoints/ProductEndpoint.fs
+++ GNogueira.SupermarketScrapeTool.API/Endpoints/ProductEndpoint.fs
@@ -1,21 +1,40 @@
 ﻿namespace GNogueira.SupermarketScrapeTool.API.Endpoints
 
 open System
+open FsToolkit.ErrorHandling
+open GNogueira.SupermarketScrapeTool.API.Clients
 open Giraffe
-open GNogueira.SupermarketScrapeTool.API.Dto
 open Microsoft.AspNetCore.Http
 
 module ProductEndpoint =
-    
     let getProductByIdHandler  (productId: Guid) (next: HttpFunc) (ctx: HttpContext) =
-        // Replace this with logic to fetch product by ID from the Cosmos DB
-        let product = { Id = productId; Name = "Sample Product"; Price = 9.99; PriceUnit = "USD" } |> Some
+        let productClient = ctx.GetService<IProductClient>()
+
+        let product =
+            productClient.GetById (productId |> string)
+            |> Async.RunSynchronously
 
         match product with
-        | Some p -> json p next ctx
-        | None -> setStatusCode 404 next ctx
-    
+        | Ok p -> json p next ctx
+        | Error e ->
+            setBodyFromString e.Message |> ignore
+            setStatusCode 404 next ctx
+
+    let getProductsPagedHandler (count: int, page: int) (next: HttpFunc) (ctx: HttpContext) =
+        let productClient = ctx.GetService<IProductClient>()
+
+        let product =
+            productClient.GetPaged count page
+            |> Async.RunSynchronously
+
+        match product with
+        | Ok p -> json p next ctx
+        | Error e ->
+            setBodyFromString e.Message |> ignore
+            setStatusCode 404 next ctx
+
     let allEndpoints : HttpHandler =
         choose [
             GET >=> routef "/products/%O" getProductByIdHandler
+            GET >=> routef "/products/paged/%i/%i" getProductsPagedHandler
         ]
