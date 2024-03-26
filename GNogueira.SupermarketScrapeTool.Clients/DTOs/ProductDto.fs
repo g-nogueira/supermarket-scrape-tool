06863d84f845e0d76e405c65b0a66e1904586cea
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2024-03-26 22:05:46 +0000

    feat(clients): update Models to v2.0.0-beta3

--- GNogueira.SupermarketScrapeTool.Clients/DTOs/ProductDto.fs
+++ GNogueira.SupermarketScrapeTool.Clients/DTOs/ProductDto.fs
@@ -2,10 +2,23 @@
 
 open System
 
+type ProductSourceDto =
+    { ExternalId: string
+      Name: string
+      Url: string
+      ImageUrl: string option }
+
+type PriceEntryDto =
+    { Date: DateTime
+      Price: float
+      PriceUnit: string
+      Source: ProductSourceDto }
+
 type ProductDto =
     { id: Guid
       Name: string
-      Source: string
-      Url: string
-      ImageUrl: string
-      updatedOn: DateTime }
+      Brand: string
+      PriceHistory: PriceEntryDto seq
+      Sources: ProductSourceDto seq
+      Ean: string option
+      UpdatedOn: DateTime }
--- GNogueira.SupermarketScrapeTool.Clients/ProductClient.fs
+++ GNogueira.SupermarketScrapeTool.Clients/ProductClient.fs
@@ -1,35 +1,78 @@
 ﻿namespace GNogueira.SupermarketScrapeTool.Clients
 
 open System
-open System.Linq.Expressions
-open FsToolkit.ErrorHandling
-open Microsoft.Azure.Cosmos
-open Microsoft.Azure.Cosmos.Linq
 open System.Linq
-open Microsoft.FSharp.Control
-open FSharpPlus
 open FSharp.Core
+open FSharpPlus
+open Microsoft.Azure.Cosmos
+open Microsoft.FSharp.Control
+open FsToolkit.ErrorHandling
 open GNogueira.SupermarketScrapeTool.Common.FSharp.CosmosDB
 open GNogueira.SupermarketScrapeTool.Common.FSharp.Queryable
-open Microsoft.FSharp.Quotations
-open Microsoft.FSharp.Linq.RuntimeHelpers.LeafExpressionConverter
+open GNogueira.SupermarketScrapeTool.Models
 
 type IProductClient =
-    abstract GetById: string -> Async<Result<ProductDto, exn>>
-    abstract GetPaged: count: int -> page: int -> Async<Result<ProductDto seq, exn>>
+    abstract GetById: string -> Async<Result<Product, exn>>
+    abstract GetPaged: count: int -> page: int -> Async<Result<Product seq, exn>>
+    abstract GetAll: unit -> Async<Result<Product seq, exn>>
     abstract GetSources: unit -> Async<Result<string seq, exn>>
-    abstract Search: count: int -> page: int -> productName: Option<string> -> supermarket: Option<string> -> updatedAfter: Option<DateTime> -> updatedBefore: Option<DateTime> -> updatedAt: Option<DateTime> -> sorting: Option<Sorting> -> Async<Result<ProductDto seq, exn>>
+    abstract Search: count: int -> page: int -> productName: Option<string> -> supermarket: Option<string> -> updatedAfter: Option<DateTime> -> updatedBefore: Option<DateTime> -> updatedAt: Option<DateTime> -> sorting: Option<Sorting> -> Async<Result<Product seq, exn>>
+
+[<AutoOpen>]
+module Extensions =
+    type ProductSource with
+
+        static member ofDto(dto: ProductSourceDto) =
+            result {
+                let! sourceName = dto.Name |> SourceName.ofString |> Result.mapError exn
+
+                return
+                    { ExternalId = dto.ExternalId
+                      Name = sourceName
+                      Url = dto.Url
+                      ImageUrl = dto.ImageUrl }
+            }
+
+    type PriceEntry with
+
+        static member ofDto(dto: PriceEntryDto) =
+            result {
+                let! priceUnit = dto.PriceUnit |> PriceUnit.ofString |> Result.mapError exn
+                let! source = dto.Source |> ProductSource.ofDto
+
+                return
+                    { Date = dto.Date
+                      Price = dto.Price
+                      PriceUnit = priceUnit
+                      Source = source }
+            }
+    type Product with
+
+       static member ofDto(dto: ProductDto) =
+            result {
+                let! priceHistory = dto.PriceHistory |> Seq.map PriceEntry.ofDto |> Seq.sequenceResultM
+                let! sources = dto.Sources |> Seq.map ProductSource.ofDto |> Seq.sequenceResultM
+                
+                return
+                    { Id = dto.id |> ProductId
+                      Name = dto.Name
+                      Brand = dto.Brand
+                      PriceHistory = priceHistory
+                      Sources = sources
+                      Ean = dto.Ean }
+            }
 
 type ProductClient(cosmosDbClient: ICosmosDbClient) =
     let productContainer = cosmosDbClient.ProductsContainer
-
+    
     interface IProductClient with
-        member this.GetById id =
+        member _.GetById id =
             productContainer.ReadItemAsync<ProductDto>(id, PartitionKey id)
             |> AsyncResult.ofTask
-            |> AsyncResult.map (fun ir -> ir.Resource)
+            |> AsyncResult.map (fun itemResponse -> itemResponse.Resource)
+            |> Async.map (Result.bind Product.ofDto)
 
-        member this.GetPaged count page =
+        member _.GetPaged count page =
             async {
                 let linqFeed =
                     productContainer.GetItemLinqQueryable<ProductDto>()
@@ -48,10 +91,30 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
 
                 let! result = readResultsAsync linqFeed []
 
-                return! result |> List.toSeq |> Ok |> Async.retn
+                return result |> List.toSeq |> Seq.map Product.ofDto |> Seq.sequenceResultM
             }
 
-        member this.Search
+        member _.GetAll () =
+            async {
+                let linqFeed =
+                    productContainer.GetItemLinqQueryable<ProductDto>()
+                    |> toFeedIterator
+
+                let rec readResultsAsync (linqFeed: FeedIterator<_>) acc =
+                    async {
+                        if linqFeed.HasMoreResults then
+                            let! response = linqFeed.ReadNextAsync() |> Async.AwaitTask
+                            return! acc @ (response.Resource |> Seq.toList) |> readResultsAsync linqFeed
+                        else
+                            return acc
+                    }
+
+                let! result = readResultsAsync linqFeed []
+
+                return result |> List.toSeq |> Seq.map Product.ofDto |> Seq.sequenceResultM
+            }
+
+        member _.Search
             (count: int)
             (page: int)
             (productName: Option<string>)
@@ -70,21 +133,21 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
 
                 let filterBySupermarket (supermarket: string option) (query: IQueryable<ProductDto>) =
                     match supermarket with
-                    | Some market -> query.Where(fun product -> product.Source.ToLower().Contains(market.ToLower()))
+                    | Some market -> query.Where(fun product -> product.Sources.Any(fun source -> source.Name.ToLower().Contains(market.ToLower())))
                     | None -> query
 
                 let filterByDateRange (updatedAfter: DateTime option) (updatedBefore: DateTime option) (updatedAt: DateTime option) (query: IQueryable<ProductDto>) =
                     let filterByUpdatedAfter (q: IQueryable<ProductDto>) =
                          match updatedAfter with
-                         | Some date -> q.Where(fun product -> product.updatedOn >= date)
+                         | Some date -> q.Where(fun product -> product.UpdatedOn >= date)
                          | None -> q
                     let filterByUpdatedBefore (q: IQueryable<ProductDto>) =
                         match updatedBefore with
-                        | Some date -> q.Where(fun product -> product.updatedOn <= date)
+                        | Some date -> q.Where(fun product -> product.UpdatedOn <= date)
                         | None -> q
                     let filterByUpdatedAt (q: IQueryable<ProductDto>) =
                         match updatedAt with
-                        | Some date -> q.Where(fun product -> product.updatedOn >= date && product.updatedOn <= date)
+                        | Some date -> q.Where(fun product -> product.UpdatedOn >= date && product.UpdatedOn <= date)
                         | None -> q
                     
                     query |> filterByUpdatedAfter |> filterByUpdatedBefore |> filterByUpdatedAt
@@ -94,7 +157,7 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
                     |> filterByName productName
                     |> filterBySupermarket supermarket
                     |> filterByDateRange updatedAfter updatedBefore updatedAt
-                    |> sort sorting <@ fun product -> product.updatedOn @>
+                    |> sort sorting <@ fun product -> product.UpdatedOn @>
                     |> skip (count * (page - 1))
                     |> take count
                     |> toFeedIterator
@@ -110,10 +173,10 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
 
                 let! result = readResultsAsync []
 
-                return! result |> List.toSeq |> Ok |> Async.retn
+                return result |> List.toSeq |> Seq.map Product.ofDto |> Seq.sequenceResultM
             }
 
-        member this.GetSources() =
+        member _.GetSources() =
             async {
                 let feedIterator = productContainer.GetItemQueryIterator<{|Source: string|}>("SELECT DISTINCT c.Source FROM c")
 
@@ -128,5 +191,5 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
 
                 let! result = readResultsAsync []
 
-                return! result |> List.toSeq |> Seq.map (fun obj -> obj.Source) |> Ok |> Async.retn
+                return! result |> List.toSeq |> Seq.map (fun obj -> obj.Source) |> AsyncResult.retn
             }
