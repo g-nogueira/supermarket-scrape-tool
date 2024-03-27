cb1eed5b7f3d2bcbe787c91622454e8975b827db
Gustavo Nogueira <gutoreis.nogueir@gmail.com>
2024-03-27 22:12:14 +0000

    feat(common): add deconstruct type helpers

--- GNogueira.SupermarketScrapeTool.Common/FSharp.fs
+++ GNogueira.SupermarketScrapeTool.Common/FSharp.fs
@@ -1,6 +1,6 @@
 ﻿namespace GNogueira.SupermarketScrapeTool.Common
 
 module FSharp =
-    let (|EmptySeq|_|) a = if Seq.isEmpty a then Some () else None
+    let (|EmptySeq|_|) a = if Seq.isEmpty a then Some() else None
 
-
+    let inline deconstruct v = (^T: (member Deconstruct: _) v)
