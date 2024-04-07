3a3eeb3df2f8b3f772faa4ba17757b1db948b201
Gustavo Nogueira <gutoreis.nogueir@gmail.coma>
2024-04-07 17:13:36 +0100

    feat(scrapper): update Start.fs to correctly upsert Products

---
+++ GNogueira.SupermarketScrapeTool.Scrapper/Models/ProductLens.fs
@@ -0,0 +1,26 @@
+﻿namespace GNogueira.SupermarketScrapeTool.Scrapper.Models
+
+open GNogueira.SupermarketScrapeTool.Common.Optics
+open GNogueira.SupermarketScrapeTool.Models
+
+[<AutoOpen>]
+module Product =
+    let Name =
+        { Get = fun product -> product.Name
+          Set = fun value product -> { product with Name = value } }
+
+    let Brand =
+        { Get = fun product -> product.Brand
+          Set = fun value product -> { product with Brand = value } }
+
+    let PriceHistory =
+        { Get = fun product -> product.PriceHistory
+          Set = fun value product -> { product with PriceHistory = value } }
+
+    let Sources =
+        { Get = fun product -> product.Sources
+          Set = fun value product -> { product with Sources = value } }
+
+    let Ean =
+        { Get = fun product -> product.Ean
+          Set = fun value product -> { product with Ean = value } }
