988c66794f5436bacbe5d4563586355ea226204a
Gustavo Nogueira <gutoreis.nogueir@gmail.coma>
2024-04-07 17:31:12 +0100

    feat(clients): update upermarketScrapeTool.Models to 2.0.0-beta10

--- GNogueira.SupermarketScrapeTool.Clients/DTOs/ProductDto.fs
+++ GNogueira.SupermarketScrapeTool.Clients/DTOs/ProductDto.fs
@@ -1,12 +1,29 @@
 ﻿namespace GNogueira.SupermarketScrapeTool.Clients
 
 open System
+open FsToolkit.ErrorHandling
+open GNogueira.SupermarketScrapeTool.Models
 
 type ProductSourceDto =
-    { ExternalId: string
-      Name: string
-      Url: string
-      ImageUrl: string option }
+    {
+        /// The id used by the Source to identify the product.
+        ProductId: string
+        Name: string
+        ProductUrl: string
+        /// The image used by the Source to identify the product.
+        ProductImageUrl: string option
+    }
+
+    static member toDomain(dto: ProductSourceDto) =
+        result {
+            let! sourceName = dto.Name |> SourceName.ofString |> Result.mapError exn
+
+            return
+                { ProductSource.ProductId = dto.ProductId
+                  Name = sourceName
+                  ProductUrl = dto.ProductUrl
+                  ProductImageUrl = dto.ProductImageUrl }
+        }
 
 type PriceEntryDto =
     { Date: DateTime
@@ -14,11 +31,45 @@ type PriceEntryDto =
       PriceUnit: string
       Source: ProductSourceDto }
 
+    static member toDomain(dto: PriceEntryDto) =
+        result {
+            let! source = dto.Source |> ProductSourceDto.toDomain
+            let! priceUnit = dto.PriceUnit |> PriceUnit.ofString |> Result.mapError exn
+
+            return
+                { PriceEntry.Date = dto.Date
+                  Price = dto.Price
+                  PriceUnit = priceUnit
+                  Source = source }
+        }
+
 type ProductDto =
-    { id: Guid
+    { id: string
       Name: string
       Brand: string
       PriceHistory: PriceEntryDto seq
       Sources: ProductSourceDto seq
-      Ean: string option
-      UpdatedOn: DateTime }
+      Ean: string }
+
+    static member toDomain(dto: ProductDto) =
+        result {
+            let! priceHistory =
+                dto.PriceHistory
+                |> Seq.map PriceEntryDto.toDomain
+                |> List.ofSeq
+                |> List.sequenceResultM
+
+            let! sources =
+                dto.Sources
+                |> Seq.map ProductSourceDto.toDomain
+                |> List.ofSeq
+                |> List.sequenceResultM
+
+            return
+                { Product.Id = dto.id |> ProductId
+                  Name = dto.Name
+                  Brand = dto.Brand
+                  PriceHistory = priceHistory
+                  Sources = sources
+                  Ean = dto.Ean }
+        }
--- GNogueira.SupermarketScrapeTool.Clients/ProductClient.fs
+++ GNogueira.SupermarketScrapeTool.Clients/ProductClient.fs
@@ -19,50 +19,6 @@ type IProductClient =
     abstract GetSources: unit -> Async<Result<string seq, exn>>
     abstract Search: count: int -> page: int -> productName: Option<string> -> supermarket: Option<string> -> updatedAfter: Option<DateTime> -> updatedBefore: Option<DateTime> -> updatedAt: Option<DateTime> -> sorting: Option<Sorting> -> Async<Result<Product seq, exn>>
 
-[<AutoOpen>]
-module Extensions =
-    type ProductSource with
-
-        static member ofDto(dto: ProductSourceDto) =
-            result {
-                let! sourceName = dto.Name |> SourceName.ofString |> Result.mapError exn
-
-                return
-                    { ExternalId = dto.ExternalId
-                      Name = sourceName
-                      Url = dto.Url
-                      ImageUrl = dto.ImageUrl }
-            }
-
-    type PriceEntry with
-
-        static member ofDto(dto: PriceEntryDto) =
-            result {
-                let! priceUnit = dto.PriceUnit |> PriceUnit.ofString |> Result.mapError exn
-                let! source = dto.Source |> ProductSource.ofDto
-
-                return
-                    { Date = dto.Date
-                      Price = dto.Price
-                      PriceUnit = priceUnit
-                      Source = source }
-            }
-    type Product with
-
-       static member ofDto(dto: ProductDto) =
-            result {
-                let! priceHistory = dto.PriceHistory |> Seq.map PriceEntry.ofDto |> Seq.sequenceResultM
-                let! sources = dto.Sources |> Seq.map ProductSource.ofDto |> Seq.sequenceResultM
-                
-                return
-                    { Id = dto.id |> ProductId
-                      Name = dto.Name
-                      Brand = dto.Brand
-                      PriceHistory = priceHistory
-                      Sources = sources
-                      Ean = dto.Ean }
-            }
-
 type ProductClient(cosmosDbClient: ICosmosDbClient) =
     let productContainer = cosmosDbClient.ProductsContainer
     
@@ -72,8 +28,8 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
 
             productContainer.ReadItemAsync<ProductDto>(productId, PartitionKey productId)
             |> AsyncResult.ofTask
-            |> AsyncResult.map (fun itemResponse -> itemResponse.Resource)
-            |> Async.map (Result.bind Product.ofDto)
+            |> AsyncResult.map (_.Resource)
+            |> Async.map (Result.bind ProductDto.toDomain)
 
         member _.GetPaged count page =
             async {
@@ -94,7 +50,7 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
 
                 let! result = readResultsAsync linqFeed []
 
-                return result |> List.toSeq |> Seq.map Product.ofDto |> Seq.sequenceResultM
+                return result |> List.toSeq |> Seq.map ProductDto.toDomain |> Seq.sequenceResultM
             }
 
         member _.GetAll () =
@@ -114,7 +70,7 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
 
                 let! result = readResultsAsync linqFeed []
 
-                return result |> List.toSeq |> Seq.map Product.ofDto |> Seq.sequenceResultM
+                return result |> List.toSeq |> Seq.map ProductDto.toDomain |> Seq.sequenceResultM
             }
 
         member _.Search
@@ -139,28 +95,28 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
                     | Some market -> query.Where(fun product -> product.Sources.Any(fun source -> source.Name.ToLower().Contains(market.ToLower())))
                     | None -> query
 
-                let filterByDateRange (updatedAfter: DateTime option) (updatedBefore: DateTime option) (updatedAt: DateTime option) (query: IQueryable<ProductDto>) =
-                    let filterByUpdatedAfter (q: IQueryable<ProductDto>) =
-                         match updatedAfter with
-                         | Some date -> q.Where(fun product -> product.UpdatedOn >= date)
-                         | None -> q
-                    let filterByUpdatedBefore (q: IQueryable<ProductDto>) =
-                        match updatedBefore with
-                        | Some date -> q.Where(fun product -> product.UpdatedOn <= date)
-                        | None -> q
-                    let filterByUpdatedAt (q: IQueryable<ProductDto>) =
-                        match updatedAt with
-                        | Some date -> q.Where(fun product -> product.UpdatedOn >= date && product.UpdatedOn <= date)
-                        | None -> q
-                    
-                    query |> filterByUpdatedAfter |> filterByUpdatedBefore |> filterByUpdatedAt
+                // let filterByDateRange (updatedAfter: DateTime option) (updatedBefore: DateTime option) (updatedAt: DateTime option) (query: IQueryable<ProductDto>) =
+                //     let filterByUpdatedAfter (q: IQueryable<ProductDto>) =
+                //          match updatedAfter with
+                //          | Some date -> q.Where(fun product -> product.UpdatedOn >= date)
+                //          | None -> q
+                //     let filterByUpdatedBefore (q: IQueryable<ProductDto>) =
+                //         match updatedBefore with
+                //         | Some date -> q.Where(fun product -> product.UpdatedOn <= date)
+                //         | None -> q
+                //     let filterByUpdatedAt (q: IQueryable<ProductDto>) =
+                //         match updatedAt with
+                //         | Some date -> q.Where(fun product -> product.UpdatedOn >= date && product.UpdatedOn <= date)
+                //         | None -> q
+                //     
+                //     query |> filterByUpdatedAfter |> filterByUpdatedBefore |> filterByUpdatedAt
 
                 let linqFeed =
                     productContainer.GetItemLinqQueryable<ProductDto>()
                     |> filterByName productName
                     |> filterBySupermarket supermarket
-                    |> filterByDateRange updatedAfter updatedBefore updatedAt
-                    |> sort sorting <@ fun product -> product.UpdatedOn @>
+                    // |> filterByDateRange updatedAfter updatedBefore updatedAt
+                    // |> sort sorting <@ fun product -> product.UpdatedOn @>
                     |> skip (count * (page - 1))
                     |> take count
                     |> toFeedIterator
@@ -176,7 +132,7 @@ type ProductClient(cosmosDbClient: ICosmosDbClient) =
 
                 let! result = readResultsAsync []
 
-                return result |> List.toSeq |> Seq.map Product.ofDto |> Seq.sequenceResultM
+                return result |> List.toSeq |> Seq.map ProductDto.toDomain |> Seq.sequenceResultM
             }
 
         member _.GetSources() =
